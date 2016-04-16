30b46f37fab47ef742552bc91b67167a55c0484c
unknown <nirajg@MININT-88GODEE.redmond.corp.microsoft.com>
2016-04-16 00:13:21 -0700

    async await sample

---
+++ SampleProject/SampleProject/asyncawaits.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    class asyncawaits
+    {
+        public void entrypoint()
+        {
+            Console.WriteLine("here1");
+            string result = DownloadContent().Result;
+            Console.WriteLine("here1");
+            Console.WriteLine(result);
+            Console.WriteLine("here2");
+            Console.Read();
+        }
+
+        public async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Console.WriteLine("here3");
+                await task1();
+                Console.WriteLine("here4");
+                return "done";
+            }
+        }
+
+        public async Task task1()
+        {
+            Thread.Sleep(1000000);
+        }
+    }
+}
